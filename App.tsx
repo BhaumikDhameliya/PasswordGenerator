1ae4a5b9bb861ab3e69021668d96b37b37728e3e
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-03-01 10:49:17 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,43 @@
+import {Button, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+
+// Form validation
+import * as Yup from 'yup';
+
+const PasswordSchema = Yup.object().shape({
+  passwordLength: Yup.number()
+    .min(4, 'Should be min of 4 characters')
+    .max(16, 'Should be max of 16 characters')
+    .required('Length is required'),
+});
+export default function App() {
+  const [password, setPassword] = useState('');
+  const [isPassGenerated, setIsPassGenerated] = useState(false);
+
+  const [lowerCase, setLowerCase] = useState(true);
+  const [upperCase, setUpperCase] = useState(false);
+  const [numbers, setNumbers] = useState('false');
+  const [symbols, setSymbols] = useState(false);
+
+  const generatePasswordString = (passwordLenght: number) => {
+    //
+  };
+
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = '';
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
+  };
+
+  return (
+    <View>
+      <Text>App</Text>
+      <Text>{numbers}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
